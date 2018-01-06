e98c1229e0cd084c9aa3b49e11d52eb290f4c1eb
bryht <bryht@live.com>
2018-01-06 23:59:55 +0100

    update readme

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # Dict
-Dictionary
+### This is a Dictionary application. ([Download](https://github.com/bryht/Dict/releases/latest))
+***
+##  Key command:
+* ###  Build install package
+> `npm run dist` 
+* ###  Publish to GitHub
+>0. set the GH_TOKEN `[Environment]::SetEnvironmentVariable("GH_TOKEN","xxxx","User")`
+>1. change version
+>2. cd to application directory and run `npm run publish`
 
-Build install package
-npm run dist 
+* ###  Check the log
+> C:\Users\\`UserName`\AppData\Roaming\dict\log.log
--- index.js
+++ index.js
@@ -13,8 +13,6 @@ const electronIsDev = require("electron-is-dev");
 autoUpdater.logger = log;
 autoUpdater.logger.transports.file.level = 'info';
 
-let win;
-
 function sendStatusToWindow(text) {
     log.info(text);
     win.webContents.send('message', text);
@@ -26,25 +24,23 @@ autoUpdater.on('update-available', (info) => {
     sendStatusToWindow('Update available.');
 })
 autoUpdater.on('update-not-available', (info) => {
-    sendStatusToWindow('Update not available.');
+    // sendStatusToWindow('Update not available.');
 })
 autoUpdater.on('error', (err) => {
     sendStatusToWindow('Error in auto-updater. ' + err);
 })
 autoUpdater.on('download-progress', (progressObj) => {
-    let log_message = "Download speed: " + progressObj.bytesPerSecond;
-    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+    let log_message = "Speed: " +parseInt(progressObj.bytesPerSecond / (1204 * 8));
+    log_message = log_message + 'KB/s  Progress:' + parseInt(progressObj.percent) + '%';
     sendStatusToWindow(log_message);
 })
 autoUpdater.on('update-downloaded', (info) => {
     sendStatusToWindow('Update downloaded');
     autoUpdater.quitAndInstall();
 });
+
+let win;
 function createWindow() {
-    if (!electronIsDev) {
-        autoUpdater.checkForUpdates();
-    }
     const _width = 1200, _height = 800
     win = new BrowserWindow({
         width: _width,
@@ -53,22 +49,15 @@ function createWindow() {
         minHeight: 600,
         icon: '../build/icon.ico'
     })
+    win.center();
+    win.loadURL(`file://${__dirname}/src/basic.html`)
+    win.on("closed", () => win = null)
     if (electronIsDev) {
         win.webContents.openDevTools();
     }
-    win.center();
-    // let menu = new Menu();
-    // menu.append(new MenuItem({
-    //     accelerator:'Return',
-    //     visible:false,
-    //     click: () => {
-    //         win.webContents.send('onKeyPress','Return');
-    //     }
-    // }));
-    // win.setMenu(null);
-
-    win.loadURL(`file://${__dirname}/src/basic.html`)
-    win.on("closed", () => win = null)
+    if (!electronIsDev) {
+        autoUpdater.checkForUpdates();
+    }
 }
 
 app.on("ready", createWindow)
