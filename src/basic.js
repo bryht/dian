ce28a40201090a5855785f3cea6b3f04fad1a229
bryht <bryht@live.com>
2018-01-18 14:45:59 +0100

    add export word

--- src/basic.js
+++ src/basic.js
@@ -8,18 +8,57 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const https = require("https");
+const fs = require("fs-extra");
 const electron_1 = require("electron");
+const storage = require("electron-json-storage");
 class Basic {
+    getAllWords() {
+        let words = new Promise((resolve, reject) => {
+            storage.get('words', function (error, data) {
+                if (error)
+                    throw error;
+                resolve(data);
+            });
+        });
+        return words;
+    }
     exportWords() {
         return __awaiter(this, void 0, void 0, function* () {
-            let filename = yield electron_1.remote.dialog.showSaveDialog({
-                'title': 'SaveWords',
-                'defaultPath': 'wordlist-result',
-                'filters': [{ 'name': 'txt', 'extensions': ['.txt'] }],
-                'buttonLabel': 'SaveWords'
-            }, filename => {
-                return filename;
+            let fileName = yield new Promise(resolve => {
+                electron_1.remote.dialog.showSaveDialog({
+                    'title': 'SaveWords',
+                    'defaultPath': 'wordlist-result',
+                    'filters': [{ 'name': 'txt', 'extensions': ['txt'] }],
+                    'buttonLabel': 'SaveWords'
+                }, result => {
+                    resolve(result);
+                });
             });
+            let folderName = fileName.split('.')[0] + "Audio";
+            let words = yield this.getAllWords();
+            let checkFolder = yield fs.ensureDir(folderName);
+            for (let index = 0; index < words.length; index++) {
+                const element = words[index];
+                let line = `${element.word},${element.type},${element.define},[${element.translation}]${element.pronunciation},${element.example}`;
+                element.word + ',' + element.define;
+                fs.appendFile(fileName, line + '\n', error => {
+                    if (error) {
+                        throw error;
+                    }
+                    console.log(line + '---Saved!');
+                });
+                this.saveMp3File(element.soundUrl, folderName + "\\" + element.word + ".mp3");
+            }
+        });
+    }
+    saveMp3File(url, fileName) {
+        var file = fs.createWriteStream(fileName);
+        file.on('finish', function () {
+            file.close(); // close() is async, call cb after close completes.
+        });
+        var request = https.get(url, res => {
+            res.pipe(file);
         });
     }
 }
--- src/basic.ts
+++ src/basic.ts
@@ -1,18 +1,59 @@
+import * as https from 'https';
 import * as fs from 'fs-extra';
 import * as readline from 'readline';
 import { remote } from 'electron';
+import * as storage from 'electron-json-storage';
+import Word from './Entities/Word';
 export class Basic {
 
+    getAllWords() {
+
+        let words = new Promise<Array<Word>>((resolve, reject) => {
+            storage.get('words', function (error, data) {
+                if (error) throw error;
+                resolve(data as Array<Word>);
+            });
+        });
+
+        return words;
+    }
 
     async exportWords() {
-        let filename = await remote.dialog.showSaveDialog({
-            'title': 'SaveWords',
-            'defaultPath': 'wordlist-result',
-            'filters': [{ 'name': 'txt', 'extensions': ['.txt'] }],
-            'buttonLabel': 'SaveWords'
-        }, filename => {
-            return filename;
-        })
+        let fileName = await new Promise<string>(resolve => {
+            remote.dialog.showSaveDialog({
+                'title': 'SaveWords',
+                'defaultPath': 'wordlist-result',
+                'filters': [{ 'name': 'txt', 'extensions': ['txt'] }],
+                'buttonLabel': 'SaveWords'
+            }, result => {
+                resolve(result);
+            });
+        });
+
+        let folderName = fileName.split('.')[0] + "Audio";
+        let words = await this.getAllWords();
+        let checkFolder=await fs.ensureDir(folderName);
+        for (let index = 0; index < words.length; index++) {
+            const element = words[index];
+            let line = `${element.word},${element.type},${element.define},[${element.translation}]${element.pronunciation},${element.example}`; element.word + ',' + element.define;
+            fs.appendFile(fileName, line + '\n', error => {
+                if (error) {
+                    throw error;
+                }
+                console.log(line + '---Saved!');
+            });
+            this.saveMp3File(element.soundUrl, folderName + "\\" + element.word + ".mp3");
+        }
+    }
+    saveMp3File(url: string, fileName: string) {
+        var file = fs.createWriteStream(fileName);
+        file.on('finish', function () {
+            file.close();  // close() is async, call cb after close completes.
+        });
+        var request = https.get(url, res => {
+            res.pipe(file);
+        });
+
     }
 
 
