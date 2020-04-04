6d3018cd110bd835bfd1132f1012147249246afb
Ming Li <ming.li@oiio.online>
2020-04-04 22:20:16 +0200

    inital

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -16,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn 
+          Learn fff
         </a>
       </header>
     </div>
